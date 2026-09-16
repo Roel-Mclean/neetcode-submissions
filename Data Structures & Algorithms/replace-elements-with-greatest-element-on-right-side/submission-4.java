9b2481b69923a99b55304f3137f0ade153764017
Roel Mclean <roel.mclean@outlook.com>
2026-09-16 15:44:32 +0100

    Add: replace-elements-with-greatest-element-on-right-side - submission-4

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        };
+        
+        int replacement = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+
+        for (int i = arr.length - 2; i > -1; i--) {
+            if (arr[i] > replacement) {
+                int tmp = arr[i];
+                arr[i] = replacement;
+                replacement = tmp;
+            } else {
+                arr[i] = replacement;
+            }
+        }
+
+        return arr;
+    }
+}
