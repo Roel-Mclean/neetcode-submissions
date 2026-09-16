3e4bdb048bb3ac061248b274e9f6d16ea5d87bc5
Roel Mclean <roel.mclean@outlook.com>
2026-09-16 15:32:23 +0100

    Add: remove-element - submission-7

---
+++ Data Structures & Algorithms/remove-element/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[--right];
+            } else {
+                left++;
+            }
+        }
+        return right;
+    }
+}
